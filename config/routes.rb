dfa9583dc24aa978a7cbd996d607d9008745b24e
Ian Johnson <tacoda@hey.com>
2025-06-05 14:23:11 +0100

    Add blog new action

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,9 @@ Rails.application.routes.draw do
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
+  get "blog_posts/new", to: "blog_posts#new", as: :new_blog_post
   get "/blog_posts/:id", to: "blog_posts#show", as: :blog_post
+  post "/blog_posts", to: "blog_posts#create", as: :blog_posts
 
   # Defines the root path route ("/")
   root "blog_posts#index"
