3813088ca5ea331249ff1e4f3b4a0ab5bc136c47
Ian Johnson <tacoda@hey.com>
2025-06-13 14:48:53 +0100

    Add published at to Blog Post

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_06_09_124340) do
+ActiveRecord::Schema[8.0].define(version: 2025_06_13_131424) do
   create_table "blog_posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "published_at"
   end
 
   create_table "users", force: :cascade do |t|
