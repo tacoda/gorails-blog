77672fd87d98a5cbc79284da6af6f1ff84f3bf94
Ian Johnson <tacoda@hey.com>
2025-06-17 15:35:24 +0100

    Add cover images

--- config/routes.rb
+++ config/routes.rb
@@ -17,7 +17,9 @@ Rails.application.routes.draw do
   # get "blog_posts/:id/edit", to: "blog_posts#edit", as: :edit_blog_post
   # post "/blog_posts", to: "blog_posts#create", as: :blog_posts
 
-  resources :blog_posts
+  resources :blog_posts do
+    resource :cover_image, only: [:destroy], module: :blog_posts
+  end
 
   # Defines the root path route ("/")
   root "blog_posts#index"
